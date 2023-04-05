8322fe74186c7c1193019faf1f5705f69557a3fe
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2023-04-05 16:35:06 +0200

    Final details (comments, responsive, clean up)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body");
 const curtain = document.querySelector(".curtain");
 
+// open curtain effect
 body.onload = function(){
     curtain.style.setProperty('--transformAfter', 'translateX(-100%)');
-
     curtain.style.setProperty('--transformBefore', 'translateX(100%)')
 }
